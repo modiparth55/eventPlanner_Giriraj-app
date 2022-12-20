b1d55f840db6fb627644106bc107f573c4740756
parth modi <modiparth88@gmail.com>
2022-12-20 13:47:13 +0530

    temp

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Events;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,9 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('calendar');
+        $events = Events::withCount('events')
+            ->get();
+
+        return view('calendar', compact('events'));
     }
 }
